5161dc60977009b69a344da6b84c3fb8a9fe5e05
Emmanuel Monle <emmanuel.monle@gmail.com>
2021-08-03 22:28:07 +0300

    pass searchResults to useEffect in App.js to reload phonebook only after a change in sealchResults

--- phonebook/src/components/contactList.js
+++ phonebook/src/components/contactList.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import phonebook from '../services/phonebook'
 
-const ContactList = ({contacts}) => {
+const ContactList = ({state}) => {
+    const contacts = state.searchResults;
+    const del = function(id){
+        phonebook.deleteContact(id);
+        const newList = state.searchResults.filter(person => person.id !== id);
+        state.setSearchResults(newList);
+        console.log(state.persons);
+        //.then(response => console.log(response))
+        //.catch(err => console.log(err))
+    }
     return(
         <div>
             {contacts.length > 0 && 
                 contacts.map(person => {
-                return <p>{person.name} {person.number}</p>
+                return <div key={person.id}>
+                        <span>{person.name} {person.number}</span>
+                        <span style={{marginLeft: '5px', color: 'red'}} onClick={()=>{del(person.id)}}>X</span>
+                    </div>
                     }
                 )
             }
